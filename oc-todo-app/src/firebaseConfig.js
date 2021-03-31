fa2d9ba0d126ac99390b4c10e90aaa9c988425da
Spydirwebb <webb.jeffery.89@gmail.com>
2021-03-31 04:52:30 +0000

    firebase_built

---
+++ oc-todo-app/src/firebaseConfig.js
@@ -0,0 +1,12 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDC72JeHJ0Rc9euV6xSibu8Xwuwk8ySD0A",
+  authDomain: "overcomplicated-todo-app-20fa3.firebaseapp.com",
+  projectId: "overcomplicated-todo-app-20fa3",
+  storageBucket: "overcomplicated-todo-app-20fa3.appspot.com",
+  messagingSenderId: "278239914070",
+  appId: "1:278239914070:web:9917ed72a27b74276bb3f7",
+  measurementId: "G-KEZN293Z6R"
+};
+
+export default firebaseConfig;
---
+++ oc-todo-app/src/index.js
@@ -0,0 +1,54 @@
+//react
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+//firebase
+import firebase from 'firebase/app'
+import "firebase/auth"
+import "firebase/firestore"
+import firebaseConfig from "./firebaseConfig";
+//redux
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import { ReactReduxFirebaseProvider } from 'react-redux-firebase'
+import { createfirestoreInstance } from 'redux-firestore'
+import { rootReducer } from "./ducks/reducers"
+//router
+import { BrowserRouter } from 'react-router-dom';
+
+const rrfConfig = {
+    userProfile: "users",
+    userFirestoreForProfile: true,
+};
+
+firebase.initializeApp(firebaseConfig);
+firebase.firestore();
+
+const initialState = {};
+const store = createStore(rootReducer, initialState);
+
+const rrfProps = {
+    firebase,
+    config: rrfConfig,
+    dispatch: store.dispatch,
+    createfirestoreInstance
+};
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+        <ReactReduxFirebaseProvider {...rrfProps}>
+            <BrowserRouter> 
+                <App />
+            </BrowserRouter>
+        </ReactReduxFirebaseProvider>
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
